d4f1998fbe17e327467ef0fd2d67465a091b029f
Steve Alomassor <stevylirious@gmail.com>
2018-05-09 11:49:33 +0200

    fix create playlist by adding scope

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,7 @@
 const clientID = 'e78e3992d1af474a98696841d4e255f8';
 const redirectURI = 'http://st3v3lyrious.surge.sh';
+// const redirectURI = 'http://localhost:3000/';
+const scope = 'playlist-modify-public';
 
 let accessToken;
 let expiresIn;
@@ -18,7 +20,7 @@ const Spotify = {
       window.history.pushState('Access Token', null, '/');
       return accessToken;
     } else {
-      window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token`;
+      window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scope=${scope}`;
     }
   },
 
@@ -64,22 +66,21 @@ const Spotify = {
         return userId = jsonResponse.id;
       }).then(()=> {
         return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-          headers: {Authorization: `Bearer ${accessToken}`, 'content-type': 'application/json'},
+          headers: {Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'},
           method: 'POST',
           body: JSON.stringify({name: name})
         }).then(response => {
           if (response.ok) {
             return response.json();
           }
-          console.log(response)
-          // throw new Error('Request Failed!');
+          throw new Error('Request Failed!');
         }, networkError => console.log(networkError.message)
         ).then(jsonResponse => {
           return playlistId = jsonResponse.id;
         });
       }).then(()=> {
         return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
-          headers: {Authorization: `Bearer ${accessToken}`, 'content-type': 'application/json'},
+          headers: {Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json'},
           method: 'POST',
           body: JSON.stringify({uris: list})
         }).then(response => {

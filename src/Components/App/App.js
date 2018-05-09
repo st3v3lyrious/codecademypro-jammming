9b4ad72b8fe3aa0857d5c67a5eeb450a3d15cd2e
Steve Alomassor <stevylirious@gmail.com>
2018-05-09 05:59:52 +0200

    fixes map error in TrackList.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,47 +9,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      searchResults : [
-        { 
-          id: 1,
-          name: '',
-          artist: '',
-          album: ''
-        },
-        {
-          id: 2,
-          name: '',
-          artist: '',
-          album: ''
-        },
-        {
-          id: 3,
-          name: '',
-          artist: '',
-          album: ''
-        }
-      ],
+      searchResults : [],
       playlistName: 'Default Playlist' ,
-      playlistTracks: [
-         { 
-          id: 1,
-          name: '',
-          artist: '',
-          album: ''
-        },
-        {
-          id: 2,
-          name: '',
-          artist: '',
-          album: ''
-        },
-        {
-          id: 3,
-          name: '',
-          artist: '',
-          album: ''
-        }
-      ]
+      playlistTracks: []
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -69,24 +31,22 @@ class App extends Component {
   }
 
   removeTrack(track) {
-    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
-      let tracks = this.state.playlistTracks;
-      tracks.pull(track);
-      this.setState({playlistTracks: tracks});
-    } else {
-      return;
-    }
+    let tracks = this.state.playlistTracks;
+    const updatedTracks = tracks.filter(playlistTrack => track.id !== playlistTrack.id);
+    this.setState({playlistTracks: updatedTracks});
   }
 
   updatePlaylistName(name) {
     this.setState({ playlistName: name });
   }
 
-  savePlaylist() {
+  savePlaylist(){
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
-    Spotify.savePlaylist(this.state.playlistName,trackURIs).then(()=> {
-        this.setState({ playlistTracks : [] });
-      }).then(()=> {this.updatePlaylistName('New Playlist ');
+    const listName = this.state.playlistName;
+    Spotify.savePlaylist(listName, trackURIs);
+    this.setState({
+      playlistName: 'New Playlist',
+      playlistTracks: []
     })
   }
 
@@ -97,13 +57,14 @@ class App extends Component {
   }
 
   render() {
+    Spotify.getAccessToken();
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
           <SearchBar onSearch={this.search} />
           <div className="App-playlist">
-            <SearchResults searchResults={this.searchResults} onAdd={this.addTrack} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} onNameChange={this.updatePlaylistName}
                       playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} 
                       onSave={this.savePlaylist} />
